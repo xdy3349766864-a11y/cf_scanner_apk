c92ab14f6b4f9f0b2a3410015ed28a6244219ea1
KISEN <xdy3349766864@gmail.com>
2026-04-10 22:58:18 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from kivy.uix.widget import Widget
 from jnius import autoclass
 from android.runnable import runOnMainThread
 
-# 显式调用 Android 原生类
 WebView = autoclass('android.webkit.WebView')
 WebViewClient = autoclass('android.webkit.WebViewClient')
 PythonActivity = autoclass('org.kivy.android.PythonActivity')
@@ -17,20 +16,11 @@ class LumoHubApp(App):
     def create_webview(self):
         activity = PythonActivity.mActivity
         webview = WebView(activity)
-        
-        # WebView 基础配置
         settings = webview.getSettings()
         settings.setJavaScriptEnabled(True)
         settings.setDomStorageEnabled(True)
-        settings.setDatabaseEnabled(True)
-        
-        # 关键：确保所有 URL 点击都在当前 WebView 加载
         webview.setWebViewClient(WebViewClient())
-        
-        # 加载目标网址
         webview.loadUrl("https://zy.3349766.xyz")
-        
-        # 将 WebView 设为当前 Activity 的主视图
         activity.setContentView(webview)
 
 if __name__ == '__main__':
